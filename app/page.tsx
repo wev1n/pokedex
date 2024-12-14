7950a0bde2549af5d97af9933a6ccc06dbdde445
wevin <kevin.warme@gmail.com>
2024-12-14 13:11:37 +0100

    refactor: duplicate fetchAllPokemon deleted from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 import { ModeToggle } from "@/components/darkmode/mode-toggle";
 import { Button } from "@/components/ui/button";
 import PokemonList from "@/components/pokemon-list";
+import { fetchAllPokemon } from "@/lib/api/fetch-all-pokemon";
 
 export default async function HomePage() {
-  const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-  if (!data.ok) {
-    throw new Error("Failed to fetch Pokémon data");
-  }
-  const pokemonList = await data.json();
-
-  console.log(pokemonList);
+  const pokemonList = await fetchAllPokemon();
 
   return (
     <main>
       <Button>Click me</Button>
       <ModeToggle />
-      <PokemonList pokemonList={pokemonList.results} />
+      <PokemonList pokemonList={pokemonList} />
     </main>
   );
 }
--- lib/api/fetch-all-pokemon.ts
+++ lib/api/fetch-all-pokemon.ts
@@ -1,5 +1,5 @@
 export async function fetchAllPokemon() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   if (!res.ok) {
     throw new Error(`Failed to fetch pokemon data: ${res.statusText}`);
   }
