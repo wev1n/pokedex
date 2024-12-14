5eb1d5b323accf9fb1e87d85455ab5e992a3a908
wevin <kevin.warme@gmail.com>
2024-12-14 12:07:05 +0100

    feat: darkmode toggler added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/darkmode/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <main>
       <Button>Click me</Button>
+      <ModeToggle />
     </main>
   );
 }
